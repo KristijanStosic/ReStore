8dbe0d7592fe52a7115831c24d8abd3bfcf031ca
Kristijan Stošić <nimeria83@yahoo.com>
2022-12-28 20:57:03 +0100

    End of section 7

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -24,25 +24,26 @@ import CheckoutPage from '../../features/checkout/CheckoutPage'
 // React toastify
 import 'react-toastify/dist/ReactToastify.css'
 
-import { getCookie } from '../util/utils'
-import { useStoreContext } from '../context/StoreContext'
 import agent from '../api/agent'
+import { getCookie } from '../util/utils'
+import { useAppDispatch } from '../store/configureStore'
+import { setBasket } from '../../features/basket/basketSlice'
 
 const App = () => {
-  const { setBasket } = useStoreContext()
+  const dispatch = useAppDispatch()
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const buyerId = getCookie('buyerId')
     if (buyerId) {
       agent.Basket.get()
-        .then(basket => setBasket(basket))
+        .then(basket => dispatch(setBasket(basket)))
         .catch(error => console.log(error))
         .finally(() => setLoading(false))
     } else {
       setLoading(false);
     }
-  }, [setBasket])
+  }, [dispatch])
 
   const [darkMode, setDarkMode] = useState(false)
   const paletteType = darkMode ? 'dark' : 'light'
--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import {
   Avatar,
   Button,
@@ -12,25 +11,17 @@ import {
 import { Link } from 'react-router-dom'
 import { Product } from '../../app/models/product'
 import { LoadingButton } from '@mui/lab'
-import { useStoreContext } from '../../app/context/StoreContext'
 import { currencyFormat } from '../../app/util/utils'
-import agent from '../../app/api/agent'
+import { useAppDispatch, useAppSelector } from '../../app/store/configureStore'
+import { addBasketItemAsync } from '../basket/basketSlice'
 
 interface Props {
   product: Product
 }
 
 export default function ProductCard({ product }: Props) {
-  const [loading, setLoading] = useState(false)
-  const { setBasket } = useStoreContext()
-
-  function handleAddItem(productId: number) {
-    setLoading(true)
-    agent.Basket.addItem(productId)
-      .then((basket) => setBasket(basket))
-      .catch((error) => console.log(error))
-      .finally(() => setLoading(false))
-  }
+  const { status } = useAppSelector(state => state.basket)
+  const dispatch = useAppDispatch()
 
   return (
     <Card>
@@ -64,8 +55,8 @@ export default function ProductCard({ product }: Props) {
       </CardContent>
       <CardActions>
         <LoadingButton
-          loading={loading}
-          onClick={() => handleAddItem(product.id)}
+          loading={status.includes('pendingAddItem' + product.id)}
+          onClick={() => dispatch(addBasketItemAsync({productId: product.id}))}
           size='small'
         >
           Add to cart
